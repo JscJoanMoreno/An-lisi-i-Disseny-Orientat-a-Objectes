9c5f8bc1dfb70866cc47dcdcddf4760e04d6708b
Jordi-DAM <21755948h@iespoblenou.org>
2025-05-13 09:52:03 +0200

    Create BookCatalogue.java

---
+++ Project/src/BookCatalogue.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCatalogue {
+    private List<Book> books;
+
+    public BookCatalogue() {
+        this.books = new ArrayList<>();
+        fillWithInitialBooks();
+    }
+
+    private void fillWithInitialBooks(){
+        //TODO
+    }
+
+
+}
