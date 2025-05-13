0c6bf986946d432846b662efa7bcd35e66269ead
Jordi-DAM <21755948h@iespoblenou.org>
2025-05-13 09:51:08 +0200

    Create MemberList.java

---
+++ Project/src/MemberList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberList {
+    private List<Member> members;
+
+    public MemberList() {
+        this.members = new ArrayList<>();
+        fillMembers();
+    }
+
+    private void fillMembers(){
+        //TODO
+    }
+}
